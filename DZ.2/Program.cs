c1b86f9e53df8e1966075a42619fa2c657107150
yuliya-gri <yuliya.grigoreva.1986@list.ru>
2022-11-26 20:21:03 +0400

    commit DZ.2

---
+++ DZ.2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Наптшите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нетю
+
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 99)
+{
+while  (num >=1000) num = num / 10;
+
+    
+    Console.WriteLine("Третья цифра");
+    Console.WriteLine(num%10);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
